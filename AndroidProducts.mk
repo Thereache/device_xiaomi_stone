46aa4ce71c4bd9158673dabda1bef993d2ce769e
Thereache <thereache2003@gmail.com>
2024-02-16 11:51:23 +0530

    stone: Blaze-ify
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The bananaOS Project
+# Copyright (C) 2023 The blazeOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_stone.mk
+    $(LOCAL_DIR)/blaze_stone.mk
 
 COMMON_LUNCH_CHOICES := \
-    banana_stone-user \
-    banana_stone-userdebug \
-    banana_stone-eng
+    blaze_stone-user \
+    blaze_stone-userdebug \
+    blaze_stone-eng
