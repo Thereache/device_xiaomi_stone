7dc90bbceafafc6762073ea39d187c96d856f097
Thereache <thereache2003@gmail.com>
2024-03-09 20:41:23 +0530

    stone: Track v4a
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,5 @@ git clone --depth=1 https://github.com/Thereache/vendor_xiaomi_stone_unified.git
 git clone --depth=1 https://github.com/Thereache/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/Thereache/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375
+
+git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
